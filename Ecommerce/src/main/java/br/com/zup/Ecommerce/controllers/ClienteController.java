791225eb4d79cdfafbda8e856bd10e511c048fd4
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-01-31 15:35:10 -0300

    feat: implementando application Properties

--- Ecommerce/src/main/java/br/com/zup/Ecommerce/controllers/ClienteController.java
+++ Ecommerce/src/main/java/br/com/zup/Ecommerce/controllers/ClienteController.java
@@ -1,42 +1,23 @@
 package br.com.zup.Ecommerce.controllers;
 
 
-import com.zup.Ecommerce.dtos.ClienteDTO;
-import com.zup.Ecommerce.dtos.ClienteRequestDTO;
-import com.zup.Ecommerce.dtos.ClienteResponseDTO;
-import com.zup.Ecommerce.services.ClienteService;
+import br.com.zup.Ecommerce.models.Cliente;
+import br.com.zup.Ecommerce.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
 
-    @RestController
-    @RequestMapping("/clientes")
-    public class ClienteController {
-
-        @Autowired
-        private ClienteService clienteService;
-
-        @PostMapping
-        public ResponseEntity<ClienteResponseDTO> criarCliente (@RequestBody ClienteRequestDTO requestDTO){
-            ClienteResponseDTO responseDTO = clienteService.criarCliente(requestDTO.getNome(), requestDTO.getEmail());
-            return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
-        }
-
-        @GetMapping
-        public List<ClienteResponseDTO> listarTodosClientes() {
-            return clienteService.listarTodosClientes();
-        }
-
-        @GetMapping("{id}")
-        public ResponseEntity<ClienteDTO> buscarClientePorId (Long id) {
-            ClienteDTO clienteDTO = clienteService.buscarClientePorId(id);
-            return ResponseEntity.ok(clienteDTO);
-        }
+    @PostMapping
+    public ResponseEntity<Cliente> cadastrarCliente(@Valid @RequestBody Cliente cliente) {
+        return ResponseEntity.ok(clienteService.cadastrarCliente(cliente));
     }
-
-
 }
