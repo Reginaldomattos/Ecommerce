fb81b708b022c5f749fa1e5ccfaa66f2fa92b7ed
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-01-31 10:25:10 -0300

    feat: implementando Cliente Service

--- Ecommerce/src/main/java/br/com/zup/Ecommerce/services/ProdutoService.java
+++ Ecommerce/src/main/java/br/com/zup/Ecommerce/services/ProdutoService.java
@@ -2,9 +2,12 @@ package br.com.zup.Ecommerce.services;
 
 import br.com.zup.Ecommerce.models.Produto;
 import br.com.zup.Ecommerce.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProdutoService {
-
+    @Autowired
     private ProdutoRepository produtoRepository;
 
     public Produto cadastrarProduto(Produto produto) {
