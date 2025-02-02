684bdf997447f1efc84f6762b2997e81a1f49d68
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-02-02 11:45:35 -0300

    feat: implementando os métodos Getters e Setters da classe Compra

--- Ecommerce/src/main/java/br/com/zup/ecommerce/models/Compra.java
+++ Ecommerce/src/main/java/br/com/zup/ecommerce/models/Compra.java
@@ -1,4 +1,48 @@
 package br.com.zup.ecommerce.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
 public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Cliente cliente;
+
+    @ManyToMany
+    private List<Produto> produtos;
+
+    private LocalDateTime dataCompra;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+
+    }
+
+    public void setId(Long Id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setDataCompra(LocalDateTime dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
 }
